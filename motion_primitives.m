879ed7963a4157d73c15d05c17a86edd4fda3cbe
g-ch <chg10086@yeah.net>
2019-03-04 21:23:25 +0800

    collision simulation

--- motion_primitives.m
+++ motion_primitives.m
@@ -1,21 +1,27 @@
-
 function [p,v,a,t,pf,vf,af] = motion_primitives(p0, v0, a0, yaw0, theta_h, theta_v, goal, d, v_max, delt_t)
+disp(p0);
+disp(v0);
+disp(a0);
 
 delt_x = d*cos(theta_v) * cos(theta_h+yaw0);
 delt_y = d*cos(theta_v) * sin(theta_h+yaw0);
 delt_z = d*sin(theta_v);
 pf = p0 + [delt_x, delt_y, delt_z];
 
-l = goal - p0;
-vf = v_max / norm(l) * l;
+l = goal - pf;
+vf = (v_max / norm(l)) * l;
 vf(3) = 0; % Note: 0 maybe better, for the p curve wont go down to meet the vf
 af = [0,0,0];
 
+disp(pf)
+disp(vf)
+
 % Choose the time as running in average velocity
-T1 = 2*delt_x/(vf(1)+v0(1));
-T2 = 2*delt_y/(vf(2)+v0(2));
-T3 = 2*delt_z/(vf(3)+v0(3));
-if  T1 > 1000
+decay_parameter = 0.5;
+T1 = 2*delt_x/(vf(1)+v0(1)) * decay_parameter;
+T2 = 2*delt_y/(vf(2)+v0(2)) * decay_parameter;
+T3 = 2*delt_z/(vf(3)+v0(3)) * decay_parameter;
+if  T1 > 1000 % eliminate infinite value
     T1 = 0;
 end
 if  T2 > 1000
@@ -36,9 +42,15 @@ for ii = 1:3 % x, y, z axis
     delt_a = af(ii) - a0(ii);
     delt_v = vf(ii) - v0(ii) - a0(ii)*T;
     delt_p = pf(ii) - p0(ii) - v0(ii)*T - 0.5*a0(ii)*T*T;
+    %   if vf is not free
     alpha = delt_a*60/T^3 - delt_v*360/T^4 + delt_p*720/T^5;
     beta = -delt_a*24/T^2 + delt_v*168/T^3 - delt_p*360/T^4;
     gamma = delt_a*3/T - delt_v*24/T^2 + delt_p*60/T^3;
+
+    %    if vf is  free
+%     alpha = -delt_a*7.5/T^3  + delt_p*45/T^5;
+%     beta = delt_a*7.5/T^2 - delt_p*45/T^4;
+%     gamma = -delt_a*1.5/T + delt_p*15/T^3;
     for jj = 1:times
         tt = t(jj);
         p(jj,ii) = alpha/120*tt^5 + beta/24*tt^4 + gamma/6*tt^3 + a0(ii)/2*tt^2 + v0(ii)*tt + p0(ii);
@@ -46,5 +58,8 @@ for ii = 1:3 % x, y, z axis
         a(jj,ii) = alpha/6*tt^3 + beta/2*tt^2 + gamma*tt + a0(ii);
     end
 end
+
+end
+
 
 
